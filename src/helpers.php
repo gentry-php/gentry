e671bb50649521589fc99eb0e98a9182e3539fc8
Marijn Ophorst <marijn@sensimedia.nl>
2016-01-29 08:23:38 +0100

    make stripping annotations optional

--- src/helpers.php
+++ src/helpers.php
@@ -17,12 +17,14 @@ function cleanOutput($string)
     return preg_replace('@\\033\[[\d;]*m@m', '', trim($string));
 }
 
-function cleanDocComment(Reflector $reflection)
+function cleanDocComment(Reflector $reflection, $strip_annotations = true)
 {
     $doccomment = $reflection->getDocComment();
     $doccomment = preg_replace("@^/\*\*@", '', $doccomment);
     $doccomment = preg_replace("@\*/$@m", '', $doccomment);
-    $doccomment = preg_replace("/^\s*\*\s*@\w+.*?$/m", '', $doccomment);
+    if ($strip_annotations) {
+        $doccomment = preg_replace("/^\s*\*\s*@\w+.*?$/m", '', $doccomment);
+    }
     $doccomment = preg_replace("@^\s*\*\s*@m", '', $doccomment);
     $doccomment = trim(preg_replace("@\s{2,}@", ' ', $doccomment));
     return $doccomment;
