33fb88e0c56157a3a9e22a0d3ce413c963c41a62
Marijn Ophorst <marijn@sensimedia.nl>
2016-01-30 09:52:13 +0100

    comment

--- src/NoTestableObjectProvidedException.php
+++ src/NoTestableObjectProvidedException.php
@@ -4,6 +4,9 @@ namespace Gentry;
 
 use DomainException;
 
+/**
+ * Exception thrown when an author forgets to specify what to test...
+ */
 class NoTestableObjectProvidedException extends DomainException
 {
 }
