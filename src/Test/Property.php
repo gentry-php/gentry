98239236a1444e45fa138509d129a2508534a1af
Marijn Ophorst <marijn@sensimedia.nl>
2016-01-30 10:20:35 +0100

    docs and handle abstract testing like methods

--- src/Test/Property.php
+++ src/Test/Property.php
@@ -5,22 +5,34 @@ namespace Gentry\Test;
 use ErrorException;
 use Exception;
 
+/**
+ * Test a property.
+ */
 class Property extends Feature
 {
+    /**
+     * Get a hash of actual property value results.
+     *
+     * @param array $args Arguments to test with.
+     * @return array A hash with value, exception and output results (if the
+     *  property is virtual, these might apply!).
+     */
     public function actual(array $args)
     {
         $actual = ['thrown' => null, 'out' => null];
         ob_start();
-        $class = $args[0];
+        $target = is_null($args[$this->target]) ?
+            $this->class :
+            $args[$this->target];
         $property = $this->name;
         try {
-            $actual['result'] = is_object($class) ?
-                $class->$property :
-                $class::$property;
+            $actual['result'] = is_object($target) ?
+                $target->$property :
+                $target::$property;
         } catch (ErrorException $e) {
             $this->messages[] = sprintf(
                 "<red>ERROR: <gray>No such property <magenta>%s::$%s",
-                is_string($class) ? $class : get_class($class),
+                is_string($target) ? $target : get_class($target),
                 $this->name
             );
         } catch (Exception $e) {
