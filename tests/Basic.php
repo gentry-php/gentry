617ff4dc345551e483963df791f844f401728498
Marijn Ophorst <marijn@sensimedia.nl>
2016-02-03 18:02:08 +0100

    modernize

--- tests/Basic.php
+++ tests/Basic.php
@@ -13,50 +13,58 @@ use ReflectionFunction;
 class Basic
 {
     /**
-     * {0}::run should successfully run a test, pipe the result and catch trimmed output
+     * {0} should successfully run a test, pipe the result and catch trimmed output
      */
-    public function testClass(Test &$test = null, $passed = 0, $failed = 0, $messages = [])
+    public function testClass(Test &$test = null)
     {
         $target = new stdClass;
         $target->test = true;
         $reflection = new ReflectionFunction(
             /**
-             * {0}::$test should be true
+             * {0} should be true
              */
             function (stdClass &$test = null) use ($target) {
                 $test = $target;
-                yield true;
+                yield 'test' => true;
             }
         );
         $test = new Test($target, $reflection);
         echo "       ";
-        yield 'is_array' => true;
+        yield 'run' => function ($passed = 0, $failed = 0, $messages = []) {
+            yield 'is_array' => true;
+        };
     }
 
     /**
-     * {0}::test should return true, and {0}::$foo should contain "bar"
+     * {0} should return true, and {0} should contain "bar"
      */
     public function testMultiple(Demo\Test $test)
     {
-        yield true;
-        yield 'bar';
+        yield 'test' => function () {
+            yield true;
+        };
+        yield 'foo' => 'bar';
     }
 
     /**
-     * {0}::test should output 4 spaces and return true
+     * {0} should output 4 spaces and return true
      */
     public function raw(Demo\Test $test)
     {
         echo '    ';
-        yield true;
+        yield 'test' => function () {
+            yield true;
+        };
     }
     
     /**
-     * {0}::aStaticMethod should be tested statically
+     * {0} should be tested statically
      */
     public function statically(Demo\Test $test = null)
     {
-        yield true;
+        yield 'aStaticMethod' => function () {
+            yield true;
+        };
     }
 }
 
