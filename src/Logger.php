7b300064b0a365e3c71d8c9af193f82322f85395
Marijn Ophorst <marijn@sensimedia.nl>
2016-02-07 14:03:31 +0100

    the logger

---
+++ src/Logger.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Gentry;
+
+class Logger
+{
+    const METHOD = 'method';
+    const FUNC = 'function';
+
+    private $logged = [];
+
+    public static function getInstance()
+    {
+        static $instance;
+        if (!isset($instance)) {
+            $instance = new static;
+        }
+        return $instance;
+    }
+
+    public function logFeature($type, $logged)
+    {
+        if (!isset($this->logged[$type])) {
+            $this->logged[$type] = [];
+        }
+        switch ($type) {
+            case self::METHOD:
+                $class = $logged[0];
+                if (!isset($this->logged[$type][$class])) {
+                    $this->logged[$type][$class] = [];
+                }
+                $this->logged[$type][$class][$logged[1]] = $logged[1];
+                break;
+        }
+    }
+
+    public function getLoggedFeatures()
+    {
+        return $this->logged;
+    }
+}
+
