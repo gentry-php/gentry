4ccca28e45c9186da7988835a37607cb935da5aa
Marijn Ophorst <marijn@sensimedia.nl>
2016-02-07 12:53:26 +0100

    rewrite/cleanup

--- src/Test.php
+++ src/Test.php
@@ -3,8 +3,10 @@
 namespace Gentry;
 
 use Reflector;
+use ReflectionClass;
 use ReflectionMethod;
 use ReflectionFunction;
+use ReflectionParameter;
 use ReflectionException;
 use zpt\anno\Annotations;
 use Exception;
@@ -12,6 +14,7 @@ use ErrorException;
 use Closure;
 use Generator;
 use SplFileInfo;
+use AssertionError;
 
 /**
  * The main test class. Normally gets constructed internally, you don't have
@@ -24,6 +27,7 @@ class Test
     private $params;
     private $annotations;
     private $description;
+    private $finally = '';
     private $feature;
     private $testtype = 'method';
     private $testedFeatures = [];
@@ -44,36 +48,10 @@ class Test
         $this->annotations = new Annotations($this->test);
         $description = cleanDocComment($this->test);
         $description = preg_replace("@\s{1,}@m", ' ', $description);
-        if (preg_match_all(
-            '@(^|[!\?,;\.]).*?{(\d+)}.*?(?=$|[!\?,;\.])@ms',
-            $description,
-            $sentences,
-            PREG_SET_ORDER
-        )) {
-            $matches = [];
-            foreach ($sentences as $sentence) {
-                $work = $sentence[0];
-                $cnt = preg_match_all(
-                    '@(.*?){(\d+)}?@ms',
-                    $work,
-                    $in_sentence,
-                    PREG_SET_ORDER
-                );
-                if ($cnt == 1) {
-                    $matches[] = $sentence;
-                } else {
-                    foreach ($in_sentence as $sub) {
-                        $matches[] = $sub;
-                        $work = str_replace($sub[0], '', $work);
-                    }
-                    if (strlen($work)) {
-                        $matches[count($matches) - 1][0] .= $work;
-                    }
-                }
-            }
-            $arguments = $this->getArguments();
-            $this->features = $matches;
-        }
+        $this->features = preg_split(
+            '@{(\?)}@',
+            $description
+        );
         $this->description = $description;
     }
 
@@ -113,89 +91,28 @@ class Test
                 $this->test->invokeArgs($this->target, $args) :
                 $runs = $this->test->invokeArgs($args);
         };
+        \Gentry\out("<blue>".array_shift($this->features));
         ob_start();
-        foreach ($invoke() as $id => $result) {
-            $out = cleanOutput(ob_get_clean());
-            if ($result instanceof Exception) {
-                $thrown = $result;
-                $result = null;
-            } else {
-                $thrown = null;
-            }
-            $expect = compact('result', 'thrown', 'out');
-            if ($feature = array_shift($this->features)) {
-                if ($result instanceof Closure
-                    && (new ReflectionFunction($result))->isGenerator()
-                ) {
-                    $result = new ReflectionFunction($result);
-                    if ($this->params[$feature[2]]->isCallable()) {
-                        $feature = new Test\ProceduralFunction(
-                            $feature,
-                            $args[$feature[2]],
-                            $result
-                        );
-                    } else {
-                        $feature = new Test\Method(
-                            $feature,
-                            $id,
-                            $this->params[$feature[2]]->getClass()->name,
-                            $result
-                        );
-                    }
-                } elseif (is_string($args[$feature[2]])) {
-                    if ($id === 'execute') {
-                        $feature = new Test\Executable(
-                            $feature,
-                            $args[$feature[2]]
-                        );
-                    }
-                } elseif (is_integer($id)) {
-                    if ($args[$feature[2]] instanceof SplFileInfo) {
-                        $id = 'getReturnedValue';
-                        $args[$feature[2]] = new File($args[$feature[2]]);
-                        $feature = new Test\ProceduralFile(
-                            $feature,
-                            $args[$feature[2]]
-                        );
-                    }
+        try {
+           foreach ($invoke() as $result) {
+                $out = cleanOutput(ob_get_clean());
+                if ($result instanceof Exception) {
+                    $thrown = $result;
+                    $result = null;
                 } else {
-                    $feature = new Test\Property(
-                        $feature,
-                        $id,
-                        $this->params[$feature[2]]->getClass()->name
-                    );
+                    $thrown = null;
                 }
-                foreach ($this->results($result, $expect) as $pipe => $exp) {
-                    $assert = $feature->assert(
-                        $args,
-                        $exp,
-                        $this->pipe($pipe, $exp['result'])
-                    );
+                out("<green>[OK]");
+                $passed++;
+                if ($feature = array_shift($this->features)) {
+                    out("<blue>$feature");
                 }
-                $tested = $feature->tested;
-                if (!isset($this->testedFeatures[$tested])) {
-                    $this->testedFeatures[$tested] = [];
-                }
-                $name = $feature->name;
-                if ($feature instanceof Test\Property) {
-                    $name = "\$$name";
-                }
-                if (!in_array($name, $this->testedFeatures[$tested])) {
-                    $this->testedFeatures[$tested][] = $name;
-                }
-                if (!$assert) {
-                    out("<red>[FAILED]\n");
-                    $messages = array_merge($messages, $feature->messages);
-                    $failed++;
-                    return;
-                } else {
-                    out("<green>[OK]");
-                    $passed++;
-                }
-            } else {
-                out("<magenta>[SKIPPED]");
+                ob_start();
             }
-            ob_start();
+        } catch (AssertionError $e) {
+            out("<red>[FAILED]");
+            $failed++;
+            $messages[] = $e->getMessage();
         }
         ob_end_clean();
         out("\n");
@@ -246,12 +163,15 @@ class Test
         $args = [];
         foreach ($this->params as $i => $param) {
             call_user_func(function () use ($param, &$args) {
-                if ($param->isDefaultValueAvailable()) {
-                    $work = $param->getDefaultValue();
-                } elseif ($type = $param->getClass()) {
-                    $work = $type->newInstance();
-                } else {
-                    $work = null;
+                $target = $this->target;
+                if ($type = $param->getClass()) {
+                    if ($type->isInternal()) {
+                        $work = $param->isDefaultValueAvailable() ?
+                            $param->getDefaultValue() :
+                            $type->newInstance();
+                    } else {
+                        $work = $this->createWrappedObject($param);
+                    }
                 }
                 $args[] =& $work;
             });
@@ -260,26 +180,177 @@ class Test
     }
 
     /**
-     * Internal method to reset arguments to pristince state and get a testable
-     * version of them (with classnames instead of null for static tests).
+     * Creates an anonymous object based on a reflection. The wrapped object
+     * proxies public methods to the actual implementation, logs their
+     * invocations and traps any exceptions.
      *
-     * @param array &$args Array of original arguments.
-     * @return array Array of testable arguments.
+     * @param ReflectionParameter $param The parameter to reflect from.
+     * @return object An anonymous, wrapped object.
      */
-    protected function resetArguments(&$args)
+    private function createWrappedObject(ReflectionParameter $param)
     {
-        $testargs = $args;
-        foreach ($args as $i => $arg) {
-            if ($this->params[$i]->isDefaultValueAvailable()) {
-                $args[$i] = $this->params[$i]->getDefaultValue();
+        $type = $param->getClass();
+        $instance = isset($this->target->{$param->name})
+            && $this->target->{$param->name} instanceof $type->name ?
+            $this->target->{$param->name} :
+            null;
+        if (isset($instance)) {
+            $type = new ReflectionClass($instance);
+        }
+        // This is nasty, but we need to dynamically extend the
+        // original class to allow type hinting to work.
+        $args = [];
+        $params = [];
+        if ($constructor = $type->getConstructor()) {
+            $params = $constructor->getParameters();
+            foreach ($params as $param) {
+                if ($param->isDefaultValueAvailable()) {
+                    $args[] = $param->getDefaultValue();
+                } elseif ($class = $param->getClass()) {
+                    $args[] = $this->createWrappedObject($param);
+                } elseif ($ptype = $param->getType()) {
+                    switch ("$ptype") {
+                        case 'array':
+                            $args[] = [];
+                            break;
+                        case 'string':
+                            $args[] = '';
+                            break;
+                        case 'int':
+                        case 'float':
+                            $args[] = 0;
+                            break;
+                        case 'callable':
+                            $args[] = function () {};
+                            break;
+                        default:
+                            $args[] = $this->createWrappedObject(
+                                new ReflectionClass("$ptype")
+                            );
+                    }
+                }
             }
         }
-        foreach ($testargs as $i => $arg) {
-            if (is_null($arg) and $class = $this->params[$i]->getClass()) {
-                $testargs[$i] = $class->name;
+        if (!isset($instance) && count($params) == count($args)) {
+            $instance = $type->newInstanceArgs($args);
+        }
+        $methods = [];
+        foreach ($type->getMethods(ReflectionMethod::IS_PUBLIC) as $method) {
+            if ($method->isInternal()) {
+                continue;
+            }
+            if ($method->name == '__construct') {
+                continue;
+            }
+            $arguments = [];
+            foreach ($method->getParameters() as $i => $param) {
+                $argument = "\$a$i";
+                if ($param->isPassedByReference()) {
+                    $argument = "&$argument";
+                }
+                if ($argtype = $param->getType()) {
+                    $argument = "$argtype $argument";
+                }
+                if ($param->isDefaultValueAvailable()) {
+                    $argument .= ' = '
+                        .$this->tostring($param->getDefaultValue());
+                }
+                $arguments["\$a$i"] = $argument;
+            }
+            $methods[] = sprintf(
+                <<<EOT
+public %1\$sfunction %2\$s(%3\$s) {
+    self::logGentryMethodCall('%2\$s'); 
+    try {
+        %4\$s
+    } catch (Throwable \$e) {
+        return \$e;
+    }
+}
+
+EOT
+                ,
+                $method->isStatic() ? 'static ' : '',
+                $method->name,
+                implode(', ', $arguments),
+                sprintf(
+                    $method->isStatic() ?
+                        'return parent::%1$s(%2$s);' :
+                        'return $this->gentryInstance->%1$s(%2$s);',
+                    $method->name,
+                    implode(', ', array_keys($arguments))
+                )
+            );
+        }
+        $methods = implode("\n", $methods);
+        $mod = '{';
+        if ($type->isTrait()) {
+            $mod = "{ use {$type->name};";
+        } elseif ($type->isInterface()) {
+            $mod = "implements {$type->name} {";
+        } else {
+            $mod = "extends {$type->name} {";
+        }
+        $work = eval(<<<EOT
+return new class $mod
+    use Gentry\ClassWrapper;
+
+    private \$gentryInstance;
+
+    public function __construct() {
+    }
+
+    $methods
+};
+EOT
+        );
+        if (isset($instance)) {
+            $work->setGentryInstance($instance);
+        }
+        return $work;
+    }
+
+    /**
+     * Internal helper method to get an echo'able representation of a random
+     * value for reporting and code generation.
+     *
+     * @param mixed $value
+     * @return string
+     */
+    private function tostring($value)
+    {
+        if (!isset($value)) {
+            return 'NULL';
+        }
+        if ($value === true) {
+            return 'true';
+        }
+        if ($value === false) {
+            return 'false';
+        }
+        if (is_scalar($value)) {
+            return $value;
+        }
+        if (is_array($value)) {
+            $out = '[';
+            $i = 0;
+            foreach ($value as $key => $entry) {
+                if ($i) {
+                    $out .= ', ';
+                }
+                $out .= $key.' => '.$this->tostring($entry);
+                $i++;
+            }
+            $out .= ']';
+            return $out;
+        }
+        if (is_object($value)) {
+            if (method_exists($value, '__toString')) {
+                return "$value";
+            } else {
+                return get_class($value);
             }
         }
-        return $testargs;
     }
 
     /**
@@ -292,52 +363,6 @@ class Test
         $_POST = [];
         $_SESSION = [];
         $_COOKIE = [];
-    }
-
-    /**
-     * Helper method to (re)attach default pipes to a testclass on a per-feature
-     * basis (since the actual `$result` isn't passed to the closures as an
-     * argument). Usually called automatically.
-     *
-     * @param mixed $pipe A possibly callable pipe.
-     * @param mixed $result Whatever result the test expects.
-     */
-    private function pipe($pipe, $result)
-    {
-        if (is_numeric($pipe)) {
-            return null;
-        }
-        switch ($pipe) {
-            case 'is_a':
-            case 'is_subclass_of':
-            case 'method_exists':
-            case 'property_exists':
-                return function ($res) use ($pipe, $result) {
-                    return call_user_func($pipe, $res, $result);
-                };
-            case 'matches':
-                return function ($res) use ($result) {
-                    return (bool)preg_match($result, $res);
-                };
-            case 'count':
-                return function ($res) use ($result) {
-                    if ($res instanceof Generator) {
-                        $i = 0;
-                        foreach ($res as $item) {
-                            $i++;
-                        }
-                        return $i;
-                    } elseif (is_array($res)) {
-                        return count($res);
-                    }
-                    return false;
-                };
-            default:
-                if (is_callable($pipe)) {
-                    return $pipe;
-                }
-                return null;
-        }
     }
 }
 
