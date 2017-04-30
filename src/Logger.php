5b42fa2453335b03526b3784985d7b97673359eb
Marijn Ophorst <marijn@monomelodies.nl>
2017-04-30 16:33:26 +0200

    use shared memory to store a serialized string of logged features

--- src/Logger.php
+++ src/Logger.php
@@ -2,6 +2,8 @@
 
 namespace Gentry\Gentry;
 
+use ErrorException;
+
 class Logger
 {
     const METHOD = 'method';
@@ -34,6 +36,17 @@ class Logger
     public function getLoggedFeatures()
     {
         return $this->logged;
+    }
+
+    public function __destruct()
+    {
+        try {
+            $shm_key = ftok(realpath(__DIR__.'/../bin').'/gentry', 't');
+            $shm = shmop_open($shm_key, 'w', 0644, 1024 * 1024);
+            shmop_write($shm, serialize($this->logged), 0);
+            shmop_close($shm);
+        } catch (ErrorException $e) {
+        }
     }
 }
 
