11791df667a005ea12a44e620b2a0d69d2f04cb0
Marijn Ophorst <marijn@monomelodies.nl>
2017-09-20 07:51:42 +0200

    fix typo

--- src/Sourcecode.php
+++ src/Sourcecode.php
@@ -139,7 +139,7 @@ class Sourcecode
      *
      * The last condition prevents potentially testable methods from external
      * sources (most likely the `vendor` directory) to be included (it should be
-     * assumet they have their own set of tests).
+     * assumed they have their own set of tests).
      *
      * @param ReflectionClass $reflection Reflected class to check methods on.
      * @return array|null An array of testable ReflectionMethods, or null if
