d54384062c12101368c963ac7b7a605cbcdf091d
Marijn Ophorst <marijn@monomelodies.nl>
2018-03-24 07:43:38 +0100

    docs and type hint

--- src/ClassWrapper.php
+++ src/ClassWrapper.php
@@ -10,11 +10,24 @@ require_once 'helpers.php';
 
 trait ClassWrapper
 {
+    /**
+     * Fake constructor. The actual construction is postponed and handled by the
+     * `ClassWrapper::__gentryConstruct` delegate.
+     *
+     * @param mixed ...$args
+     * @return void
+     */
     public function __construct(...$args)
     {
     }
 
-    public function __gentryConstruct(...$args)
+    /**
+     * Constructor delegate.
+     *
+     * @param mixed ...$args
+     * @return void
+     */
+    public function __gentryConstruct(...$args) : void
     {
         try {
             if (method_exists(get_parent_class($this), '__construct')) {
@@ -24,6 +37,16 @@ trait ClassWrapper
         }
     }        
 
+    /**
+     * Logs a method call so Gentry can determine untested methods. Mostly used
+     * internally by gentry.
+     *
+     * @param string $method Method name
+     * @param string|null $class Optional classname. Default to the parent of
+     *  the current class.
+     * @param array $args Optional array of arguments.
+     * @return void
+     */
     public static function __gentryLogMethodCall(string $method, string $class = null, array $args = []) : void
     {
         if (!$class) {
