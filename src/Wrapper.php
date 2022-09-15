4513ce02dbf6d107ff5ea9af2ccc054cec98b8fa
Marijn Ophorst <marijn@monomelodies.nl>
2022-09-15 15:47:54 +0200

    add reflection so we can also access private/protected methods/properties

--- src/Wrapper.php
+++ src/Wrapper.php
@@ -2,30 +2,38 @@
 
 namespace Gentry\Gentry;
 
+use ReflectionMethod;
+use ReflectionProperty;
+
 /**
  * Wrapper class for logging method calls.
  */
 class Wrapper
 {
-    private static $staticWrapped;
-
-    public function __construct(private object $wrapped)
-    {
-        self::$staticWrapped = get_class($wrapped);
-    }
+    public function __construct(
+        private object|string $wrapped
+    ) {}
 
     public function __call(string $method, array $args) : mixed
     {
         $logger = Logger::getInstance();
         $logger->logFeature($this->wrapped, $method, $args);
-        return $this->wrapped->$method(...$args);
+        $method = new ReflectionMethod($this->wrapped, $method);
+        $method->setAccessible(true);
+        return $method->invoke($this->wrapped, ...$args);
     }
 
     public static function __callStatic(string $method, array $args) : mixed
     {
-        $logger = Logger::getInstance();
-        $logger->logFeature(self::$staticWrapped, $method, $args);
-        return self::$staticWrapped::$method(...$args);
+        // Since we use reflection, this is now identical.
+        return $this->__call($method, $args);
+    }
+
+    public function __get(string $name) : mixed
+    {
+        $property = new ReflectionProperty($this->wrapped, $name);
+        $property->setAccessible(true);
+        return $property->getValue($this->wrapped);
     }
 }
 
