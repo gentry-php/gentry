057f6b19dee76b0c9239c8377d2ddf82de295abe
Marijn Ophorst <marijn@monomelodies.nl>
2022-09-23 14:27:35 +0200

    internalize the whole shmop stuff

--- src/Logger.php
+++ src/Logger.php
@@ -4,6 +4,7 @@ namespace Gentry\Gentry;
 
 use ErrorException;
 use Monomelodies\Reflex\ReflectionMethod;
+use Shmop;
 
 class Logger
 {
@@ -77,12 +78,49 @@ class Logger
      */
     public function __destruct()
     {
-        try {
-            $shm_key = ftok(realpath(__DIR__).'/Command.php', 't');
-            $shm = shmop_open($shm_key, 'w', 0644, 1024 * 1024);
-            shmop_write($shm, serialize($this->logged), 0);
-            shmop_close($shm);
-        } catch (ErrorException $e) {
+        self::write(serialize($this->logged));
+    }
+
+    /**
+     * Read data from shared memory.
+     *
+     * @return string
+     */
+    public static function read() : string
+    {
+        $shm = self::getShmHandle();
+        $data = shmop_read($shm, 0, 1024 * 1024);
+        shmop_delete($shm);
+        return $data;
+    }
+
+    /**
+     * Write data to shared memory.
+     *
+     * @param string $data
+     * @return void
+     */
+    private static function write(string $data) : void
+    {
+        $shm = self::getShmHandle();
+        shmop_write($shm, $data, 0);
+        shmop_close($shm);
+    }
+
+    /**
+     * Get a handle to the shared memory block.
+     *
+     * @return Shmop
+     * @throws Gentry\Gentry\ErrorGettingSharedMemoryBlockException
+     */
+    private static function getShmHandle() : Shmop
+    {
+        $shm_key = ftok(realpath(__FILE__), 't');
+        $shm = shmop_open($shm_key, 'w', 0644, 1024 * 1024);
+        if ($shm) {
+            return $shm;
+        } else {
+            throw new ErrorGettingSharedMemoryBlockException;
         }
     }
 }
