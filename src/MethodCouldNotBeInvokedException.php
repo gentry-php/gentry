113e5d2de420cf15ffa5b6d212e3a4a050f23480
Marijn Ophorst <marijn@sensimedia.nl>
2023-02-10 16:12:32 +0100

    allow previous exception to be passed along

--- src/MethodCouldNotBeInvokedException.php
+++ src/MethodCouldNotBeInvokedException.php
@@ -3,13 +3,14 @@
 namespace Gentry\Gentry;
 
 use DomainException;
+use Throwable;
 
 class MethodCouldNotBeInvokedException extends DomainException
 {
-    public function __construct(object|string $wrapped, string $method)
+    public function __construct(object|string $wrapped, string $method, Throwable $previous = null)
     {
         $class = is_object($wrapped) ? get_class($wrapped) : $wrapped;
-        parent::__construct("The method $class::$method could not be invoked");
+        parent::__construct("The method $class::$method could not be invoked", 0, $previous);
     }
 }
 
