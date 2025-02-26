60625824249ef68410bec4e9c0e535a554ded3f5
Marijn Ophorst <marijn@sensimedia.nl>
2025-02-26 15:57:26 +0100

    add explicit nullable type, and some cleanup

--- src/MethodCouldNotBeInvokedException.php
+++ src/MethodCouldNotBeInvokedException.php
@@ -7,11 +7,14 @@ use Throwable;
 
 class MethodCouldNotBeInvokedException extends DomainException
 {
-    public function __construct(object|string $wrapped, string $method, Throwable $previous = null)
+    public function __construct(object|string $wrapped, string $method, ?Throwable $previous = null)
     {
         $class = is_object($wrapped) ? get_class($wrapped) : $wrapped;
         parent::__construct(
-            sprintf("The method $class::$method could not be invoked, with error %s", $previous ? $previous->getMessage() : '[unknown]'),
+            sprintf(
+                "The method $class::$method could not be invoked, with error %s",
+                $previous ? $previous->getMessage() : '[unknown]'
+            ),
             0,
             $previous
         );
